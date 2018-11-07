2199b95c677b3f47688e3ecef803136a6a0aa24b
mrmrs <hi@mrmrs.cc>
2018-11-07 10:57:15 +0000

    Port element from glamorous to emotion

--- elements/H3.js
+++ elements/H3.js
@@ -5,7 +5,7 @@ import {
   color, borders, borderColor, borderWidth,
 } from 'styled-system'
 
-const H3 = glamorous.h3(space, display, width, alignItems, justifyContent, fontSize, fontWeight, lineHeight, color, borders, borderColor, borderWidth, {
+const H3 = styled('h3')(space, display, width, alignItems, justifyContent, fontSize, fontWeight, lineHeight, color, borders, borderColor, borderWidth, {
   boxSizing: 'border-box'
 })
 
