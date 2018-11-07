2d218a57183f680dc04a63d50f47441697a32eb2
mrmrs <hi@mrmrs.cc>
2018-11-07 10:51:57 +0000

    Change import from glamorous to react-emotion

--- elements/Ul.js
+++ elements/Ul.js
@@ -1,4 +1,4 @@
-import glamorous from 'glamorous'
+import styled from 'react-emotion'
 import { space, display, width, fontSize, lineHeight, color, borders, borderColor, borderWidth, borderRadius } from 'styled-system'
 
 const Ul = glamorous.ul(space, display, width, fontSize, lineHeight, color, borders, borderColor, borderWidth, borderRadius, {
