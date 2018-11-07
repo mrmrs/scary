b6cd7c3296c4bbbbef0f7a80424d239ea563e732
mrmrs <hi@mrmrs.cc>
2018-11-07 10:51:53 +0000

    Change import from glamorous to react-emotion

--- elements/Pre.js
+++ elements/Pre.js
@@ -1,4 +1,4 @@
-import glamorous from 'glamorous'
+import styled from 'react-emotion'
 import { space, display, width, fontSize, color } from 'styled-system'
 
 const Pre = glamorous.pre(space, display, width, fontSize, color, {
