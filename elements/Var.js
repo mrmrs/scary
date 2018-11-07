787d329f6fe3ca5078a0d7b361c5d2dd9f78532a
mrmrs <hi@mrmrs.cc>
2018-11-07 10:57:23 +0000

    Port element from glamorous to emotion

--- elements/Var.js
+++ elements/Var.js
@@ -2,7 +2,7 @@ import styled from 'react-emotion'
 import { space, width, fontSize, color } from 'styled-system'
 import theme from '../theme'
 
-const Var = glamorous.var(space, width, fontSize, color, {
+const Var = styled('var')(space, width, fontSize, color, {
   fontFamily: theme.fontFamily.mono
 })
 
