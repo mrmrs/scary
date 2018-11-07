41c4f45c36654706e3774b65ce035639ebca785f
mrmrs <hi@mrmrs.cc>
2018-11-07 10:57:17 +0000

    Port element from glamorous to emotion

--- elements/Legend.js
+++ elements/Legend.js
@@ -1,7 +1,7 @@
 import styled from 'react-emotion'
 import { space, display, width, fontSize, color, borders, borderColor, borderWidth, borderRadius } from 'styled-system'
 
-const Legend = glamorous.legend(space, display, width, fontSize, color, borders, borderColor, borderWidth, borderRadius, {
+const Legend = styled('legend')(space, display, width, fontSize, color, borders, borderColor, borderWidth, borderRadius, {
   boxSizing: 'border-box'
 })
 
