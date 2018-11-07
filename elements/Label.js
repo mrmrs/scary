dde089a7cb4b27cb3b051292658fb717c7731c64
mrmrs <hi@mrmrs.cc>
2018-11-07 10:51:51 +0000

    Change import from glamorous to react-emotion

--- elements/Label.js
+++ elements/Label.js
@@ -1,4 +1,4 @@
-import glamorous from 'glamorous'
+import styled from 'react-emotion'
 import { space, display, width, fontSize, fontWeight, lineHeight, textAlign, color } from 'styled-system'
 
 const Label = glamorous.label(space, display, width, fontSize, fontWeight, lineHeight, textAlign, color, {
