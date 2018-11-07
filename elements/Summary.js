b2f8feed8086305e0cabbbc51dd5bad2bb37c878
mrmrs <hi@mrmrs.cc>
2018-11-07 10:51:55 +0000

    Change import from glamorous to react-emotion

--- elements/Summary.js
+++ elements/Summary.js
@@ -1,4 +1,4 @@
-import glamorous from 'glamorous'
+import styled from 'react-emotion'
 import { space, display, width, fontSize, fontWeight, textAlign, color } from 'styled-system'
 
 const Summary = glamorous.summary(space, display, width, fontSize, fontWeight, textAlign, color, {
