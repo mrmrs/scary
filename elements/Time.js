5cc03f607619c4fa379d827d9c0e639d8e5f59ae
mrmrs <hi@mrmrs.cc>
2018-11-07 10:51:57 +0000

    Change import from glamorous to react-emotion

--- elements/Time.js
+++ elements/Time.js
@@ -1,4 +1,4 @@
-import glamorous from 'glamorous'
+import styled from 'react-emotion'
 import { space, display, width, fontSize, fontWeight, lineHeight, color } from 'styled-system'
 
 const Time = glamorous.time(space, display, width, fontSize, fontWeight, lineHeight, color, {
