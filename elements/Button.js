9cee40f3ca7a5784688e1c23c4577f3993d50839
mrmrs <hi@mrmrs.cc>
2018-11-07 10:57:12 +0000

    Port element from glamorous to emotion

--- elements/Button.js
+++ elements/Button.js
@@ -6,7 +6,7 @@ import {
   hover, active, focus 
 } from 'styled-system'
 
-const Button = glamorous.button(space, width, display, alignItems, justifyContent, fontSize, fontWeight, lineHeight, color, borders, borderColor, borderWidth, borderRadius, hover, active, focus, { 
+const Button = styled('button')(space, width, display, alignItems, justifyContent, fontSize, fontWeight, lineHeight, color, borders, borderColor, borderWidth, borderRadius, hover, active, focus, { 
   boxSizing: 'border-box', 
   overflow: 'visible',
   textTransform: 'none',
