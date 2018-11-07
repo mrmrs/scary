c905382fde9173e83bd11c504bdfa71f4390876d
mrmrs <hi@mrmrs.cc>
2018-11-07 10:57:20 +0000

    Port element from glamorous to emotion

--- elements/Sub.js
+++ elements/Sub.js
@@ -1,7 +1,7 @@
 import styled from 'react-emotion'
 import { space, fontSize, fontWeight, lineHeight, color } from 'styled-system'
 
-const Sub = glamorous.sub(space, fontSize, fontWeight, lineHeight, color, {
+const Sub = styled('sub')(space, fontSize, fontWeight, lineHeight, color, {
 
 })
 
