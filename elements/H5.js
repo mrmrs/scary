09e49bc3676bf504253451f50c7baf6b9c5e84f7
mrmrs <hi@mrmrs.cc>
2018-11-07 10:57:15 +0000

    Port element from glamorous to emotion

--- elements/H5.js
+++ elements/H5.js
@@ -5,7 +5,7 @@ import {
   color, borders, borderColor, borderWidth,
 } from 'styled-system'
 
-const H5 = glamorous.h5(space, width, display, alignItems, justifyContent, fontSize, fontWeight, lineHeight, color, borders, borderColor, borderWidth, {
+const H5 = styled('h5')(space, width, display, alignItems, justifyContent, fontSize, fontWeight, lineHeight, color, borders, borderColor, borderWidth, {
   boxSizing: 'border-box'
 })
 
