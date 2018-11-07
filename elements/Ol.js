e38ebc52de70c872cbf6ec070f0f03f6cf087706
mrmrs <hi@mrmrs.cc>
2018-11-07 10:57:18 +0000

    Port element from glamorous to emotion

--- elements/Ol.js
+++ elements/Ol.js
@@ -1,7 +1,7 @@
 import styled from 'react-emotion'
 import { space, width, display, fontSize, color, borders, borderColor, borderWidth, borderRadius } from 'styled-system'
 
-const Ol = glamorous.ol(space, width, display, fontSize, color, borders, borderColor, borderWidth, borderRadius, {
+const Ol = styled('ol')(space, width, display, fontSize, color, borders, borderColor, borderWidth, borderRadius, {
   boxSizing: 'border-box'
 })
 
