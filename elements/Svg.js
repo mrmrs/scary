a418b4b4015fd39e4b9ae0669cfa04caa3aea508
mrmrs <hi@mrmrs.cc>
2018-11-07 10:51:55 +0000

    Change import from glamorous to react-emotion

--- elements/Svg.js
+++ elements/Svg.js
@@ -1,4 +1,4 @@
-import glamorous from 'glamorous'
+import styled from 'react-emotion'
 import { space, display, width, color } from 'styled-system'
 
 const Svg = glamorous.svg(space, display, width, color, {
