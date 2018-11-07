10630e7942d2ee1676ee9b4d480e805afc13437e
mrmrs <hi@mrmrs.cc>
2018-11-07 10:57:22 +0000

    Port element from glamorous to emotion

--- elements/Time.js
+++ elements/Time.js
@@ -1,7 +1,7 @@
 import styled from 'react-emotion'
 import { space, display, width, fontSize, fontWeight, lineHeight, color } from 'styled-system'
 
-const Time = glamorous.time(space, display, width, fontSize, fontWeight, lineHeight, color, {
+const Time = styled('time')(space, display, width, fontSize, fontWeight, lineHeight, color, {
 
 })
 
