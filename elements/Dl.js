dd2eb24e7e1e4d59d8671637d2d852722c5c7011
mrmrs <hi@mrmrs.cc>
2018-11-07 10:57:13 +0000

    Port element from glamorous to emotion

--- elements/Dl.js
+++ elements/Dl.js
@@ -5,7 +5,7 @@ import {
   color, borders, borderColor, borderWidth, borderRadius 
 } from 'styled-system'
 
-const Dl = glamorous.dl(space, display, width, fontSize, lineHeight, color, borders, borderColor, borderWidth, borderRadius, {
+const Dl = styled('dl')(space, display, width, fontSize, lineHeight, color, borders, borderColor, borderWidth, borderRadius, {
 
 })
 
