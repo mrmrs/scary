d737cafa85446bfa27b9db1a5501aba17162fd4a
mrmrs <hi@mrmrs.cc>
2018-11-07 10:57:18 +0000

    Port element from glamorous to emotion

--- elements/Nav.js
+++ elements/Nav.js
@@ -1,7 +1,7 @@
 import styled from 'react-emotion'
 import { space, display, width, alignItems, justifyContent, wrap, fontSize, color, borders, borderColor, borderWidth, borderRadius } from 'styled-system'
 
-const Nav = glamorous.nav(space, display, width, alignItems, justifyContent, wrap, fontSize, color, borders, borderColor, borderWidth, borderRadius, {
+const Nav = styled('nav')(space, display, width, alignItems, justifyContent, wrap, fontSize, color, borders, borderColor, borderWidth, borderRadius, {
   boxSizing: 'border-box'
 })
 
