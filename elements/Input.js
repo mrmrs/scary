5b4268bfe075849f41bb7fbb9d56a739b754d319
mrmrs <hi@mrmrs.cc>
2018-11-07 10:51:51 +0000

    Change import from glamorous to react-emotion

--- elements/Input.js
+++ elements/Input.js
@@ -1,4 +1,4 @@
-import glamorous from 'glamorous'
+import styled from 'react-emotion'
 import { space, display, width, fontSize, borders, borderColor, borderWidth, borderRadius, color, hover, active, focus } from 'styled-system'
 
 const Input = glamorous.input(space, display, width, fontSize, color, borders, borderColor, borderWidth, borderRadius, hover, active, focus, {
