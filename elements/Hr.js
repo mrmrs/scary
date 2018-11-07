2610d7f86fc339b313c923e3fda2bdc588270758
mrmrs <hi@mrmrs.cc>
2018-11-07 10:51:49 +0000

    Change import from glamorous to react-emotion

--- elements/Hr.js
+++ elements/Hr.js
@@ -1,4 +1,4 @@
-import glamorous from 'glamorous'
+import styled from 'react-emotion'
 import { space, width, height, color, borders, borderColor, borderWidth } from 'styled-system'
 
 const Hr = glamorous.hr(space, width, height, color, borders, borderColor, borderWidth, {
