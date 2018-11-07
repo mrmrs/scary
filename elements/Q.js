dfc5b94271c0eb97bdb69043077d861995330338
mrmrs <hi@mrmrs.cc>
2018-11-07 10:51:53 +0000

    Change import from glamorous to react-emotion

--- elements/Q.js
+++ elements/Q.js
@@ -1,4 +1,4 @@
-import glamorous from 'glamorous'
+import styled from 'react-emotion'
 import { space, width, fontSize, fontWeight, lineHeight, color } from 'styled-system'
 
 const Q = glamorous.q(space, width, fontSize, fontWeight, lineHeight, color, {
