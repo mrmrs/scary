c7628a188fcf5bd91a6c20940b9599cb4d269310
mrmrs <hi@mrmrs.cc>
2018-11-07 10:51:55 +0000

    Change import from glamorous to react-emotion

--- elements/Sub.js
+++ elements/Sub.js
@@ -1,4 +1,4 @@
-import glamorous from 'glamorous'
+import styled from 'react-emotion'
 import { space, fontSize, fontWeight, lineHeight, color } from 'styled-system'
 
 const Sub = glamorous.sub(space, fontSize, fontWeight, lineHeight, color, {
