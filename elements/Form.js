0cad6b9941fb1639213d3b13e9a136b744de1f29
mrmrs <hi@mrmrs.cc>
2018-11-07 10:57:14 +0000

    Port element from glamorous to emotion

--- elements/Form.js
+++ elements/Form.js
@@ -5,7 +5,7 @@ import {
   color, borders, borderColor, borderWidth, borderRadius 
 } from 'styled-system'
 
-const Form = glamorous.form(space, display, width, fontSize, color, borders, borderColor, borderWidth, borderRadius, {
+const Form = styled('form')(space, display, width, fontSize, color, borders, borderColor, borderWidth, borderRadius, {
   boxSizing: 'border-box'
 })
 
