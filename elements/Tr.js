5431fc2a96d813d46caab520a0a76ea0481e3b5a
mrmrs <hi@mrmrs.cc>
2018-11-07 10:51:57 +0000

    Change import from glamorous to react-emotion

--- elements/Tr.js
+++ elements/Tr.js
@@ -1,4 +1,4 @@
-import glamorous from 'glamorous'
+import styled from 'react-emotion'
 import { space, width, fontSize, color } from 'styled-system'
 
 const Tr = glamorous.tr(space, width, fontSize, color, {
