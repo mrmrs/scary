cd99af2f975f17d916f81a2fb699063455fa67f3
mrmrs <hi@mrmrs.cc>
2018-11-07 10:51:48 +0000

    Change import from glamorous to react-emotion

--- elements/H1.js
+++ elements/H1.js
@@ -1,4 +1,4 @@
-import glamorous from 'glamorous'
+import styled from 'react-emotion'
 import { 
   space, display, width, alignItems, justifyContent, 
   fontSize, fontWeight, lineHeight, textAlign,
