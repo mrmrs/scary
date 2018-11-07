e430a805ae58d024a320bab4bcf12883ada50b7c
mrmrs <hi@mrmrs.cc>
2018-11-07 10:51:54 +0000

    Change import from glamorous to react-emotion

--- elements/Span.js
+++ elements/Span.js
@@ -1,4 +1,4 @@
-import glamorous from 'glamorous'
+import styled from 'react-emotion'
 import { space, display, width, fontSize, fontWeight, lineHeight, textAlign, color } from 'styled-system'
 
 const Span = glamorous.span(space, display, width, fontSize, fontWeight, lineHeight, textAlign, color, {
