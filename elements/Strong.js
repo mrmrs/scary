1cb9f458574b7fa4aaf59c2770d242fbeff93522
mrmrs <hi@mrmrs.cc>
2018-11-07 10:51:55 +0000

    Change import from glamorous to react-emotion

--- elements/Strong.js
+++ elements/Strong.js
@@ -1,4 +1,4 @@
-import glamorous from 'glamorous'
+import styled from 'react-emotion'
 import { space, fontSize, fontWeight, color } from 'styled-system'
 
 const Strong = glamorous.strong(space, fontSize, fontWeight, color, {
