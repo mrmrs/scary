13af2fc994f7a90a4e9cec7d48b1a4c101ad1f3e
mrmrs <hi@mrmrs.cc>
2019-01-13 17:52:22 -0500

    Remove need to manually include elements or components

--- next.config.js
+++ next.config.js
@@ -1,9 +1,24 @@
 const images = require('remark-images')
 const emoji = require('remark-emoji')
+const webpack = require("webpack")
+const path = require("path")
+const fs = require("fs-extra")
+
+const fetchFiles = async filePath => {
+  const files = await fs.readdir(filePath)
+
+  return files.reduce((acc, file) => {
+    const elName = path.basename(file, ".js")
+    return {
+      ...acc,
+      [elName]: [`${filePath}/${file}`, "default"]
+    }
+  }, {})
+}
 
 module.exports = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-  webpack: (config, { defaultLoaders }) => {
+  webpack: async (config, { defaultLoaders }) => {
     config.module.rules.push({
       test: /\.mdx?$/,
       use: [
@@ -16,6 +31,18 @@ module.exports = {
         }
       ]
     })
+    const elements = await fetchFiles(path.join(__dirname, "elements"))
+    const components = await fetchFiles(path.join(__dirname, "components"))
+    config.plugins.push(
+      new webpack.ProvidePlugin(elements),
+      new webpack.ProvidePlugin(components)
+    )
+    config.resolve = {
+      alias: {
+        elements: path.resolve(__dirname, "elements/"),
+        components: path.resolve(__dirname, "components/")
+      }
+    }
 
     return config
   }
