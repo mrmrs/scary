9ac48c72ced3fa7926470402a2c5d0d70a9c6d55
mrmrs <hi@mrmrs.cc>
2019-01-13 18:14:33 -0500

    Changing colors. Removing manual imports.

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,5 @@
 import React from 'react'
 
-import Div from '../elements/Div'
-import H1 from '../elements/H1'
-import P from '../elements/P'
-import A from '../elements/A'
-
-
-import theme from '../theme'
-
 const Home = props => (
     <Div>
       <H1 fontWeight={600}>Title</H1>
